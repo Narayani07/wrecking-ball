1a68f96c4f649208ced757d90a7c1c611436cf82
Narayani07 <77108317+Narayani07@users.noreply.github.com>
2021-04-15 19:02:50 +0530

    Add files via upload

---
+++ wrekingball/Ball.js
@@ -0,0 +1,24 @@
+class Ball{
+  constructor(x, y, width, height,angle) {
+      var options = {
+          frictionAir:1.0,
+          density:1
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+    
+      World.add(world, this.body);
+    }
+    display(){
+      var angle = this.body.angle;
+      var pos = this.body.position;
+      push();
+      fill("sea green");
+      translate(pos.x,pos.y);
+      rotate(angle);
+      rectMode(CENTER);
+      ellipse( 0, 0, this.width, this.height);
+      pop();
+    }
+}
